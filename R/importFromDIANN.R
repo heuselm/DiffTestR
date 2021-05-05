a50fe84f3de9cbb9b523e57d556b61a63e4c459b
heuselm <heuselmo@gmx.de>
2021-05-05 15:40:37 +0200

    add study design template writing functionality

--- R/importFromDIANN.R
+++ R/importFromDIANN.R
@@ -6,6 +6,8 @@
 #' @param quant_value_col The name of the column containing precursor level quant
 #' values to use. Typically Precursor.Quantity or Precursor.Normalised
 #' @param study_design Study design in tab-separated .txt with mandatory columns filename, condition, replicate
+#' By default, Study_design_template.tsv is written next to the input tsv. Fill in condition and replicate and save as
+#' Study_design_filled.tsv. Then the next run of the function will likely go through ;-).
 #'
 #' @return A qdata result object (list) containing objects data, data_wide, matrix_raw, ann_col and ann_row
 #'
@@ -15,8 +17,9 @@
 #' @export
 #'
 importFromDIANN <- function(path_to_result = "../../04_DIANN_LfMS1/DIANN_LfMS1-Pass2.tsv",
-                              quant_value_col = "Precursor.Quantity",
-                              study_design = "../../04_DIANN_LfMS1/Study_design.txt")
+                            quant_value_col = "Precursor.Quantity",
+                            write_study_design_template = TRUE,
+                            study_design = "../../04_DIANN_LfMS1/Study_design.txt")
 {
   # generate container object
   qdata = list()
@@ -25,16 +28,29 @@ importFromDIANN <- function(path_to_result = "../../04_DIANN_LfMS1/DIANN_LfMS1-P
   data = data.table::fread(path_to_result)
 
   # flatten filenames
-  data[, File.Name:=unlist(strsplit(File.Name, split = "\\\\"))[length(unlist(strsplit(File.Name, split = "\\\\")))], File.Name]
-  data[, File.Name:=gsub(".raw", "", File.Name)]
+  data[, File.Name:=unlist(strsplit(File.Name, split = "\\\\"))
+       [length(unlist(strsplit(File.Name, split = "\\\\")))], File.Name]
+  data[, File.Name:=gsub(".raw|.d", "", File.Name)]
+
+  # write study_design template based on the filenames in the dataset if selected
+
+  if (write_study_design_template){
+    # write study_design template based on the filenames in the dataset
+    nruns = length(unique(data$File.Name))
+    study_des_template = data.table("filename" = unique(data$File.Name),
+                                    "condition" = rep("FILL INN", nruns),
+                                    "replicate" = rep("FILL INN", nruns))
+    fwrite(study_des_template, file = paste0(dirname(path_to_result),"/Study_design_template.tsv"), sep = "\t")
+
+    study_des = data.table::fread(paste0(dirname(path_to_result),"/Study_design_filled.tsv"))
+  } else if (is.character(study_design)){
+    study_des = fread(study_design) # Assuming a path to a different study design file has been provided
+  } else {
+      study_des = study_design
+    }
 
   # keep only data for files in Study design
-  study_des = data.table::fread(study_design)
-
-  # study_des$filename = paste(study_des$filename, "012")
-
   data.s = data[File.Name %in% study_des$filename]
-
   if (nrow(data.s) == 0){
     message("filenames in data: ", cat(unique(data$File.Name)), "\n")
     message("filenames in ", study_design, ": ", cat(unique(study_des$filename)))
@@ -47,16 +63,16 @@ importFromDIANN <- function(path_to_result = "../../04_DIANN_LfMS1/DIANN_LfMS1-P
   # Reformat to wide, save as matrix and assemble matrix row and column annotations
   data.s.wide = data.table::dcast(data.s, Protein.Group+Protein.Ids+Protein.Names+Genes+Modified.Sequence+Stripped.Sequence+Precursor.Id~File.Name+condition+replicate, value.var = quant_value_col)
     data.s.wide.m.raw = as.matrix(data.s.wide[, 8:ncol(data.s.wide)])
-  row.names(data.s.wide.m.raw) = data.wide$Precursor.Id
+  row.names(data.s.wide.m.raw) = data.s.wide$Precursor.Id
 
   # assemble annotations
-  ann_col = study_design
+  ann_col = study_des
   ann_col[, rn:=paste0(filename,condition,replicate, sep = "_"), filename]
   row.names(ann_col) = ann_col$rn
   ann_col$rn = NULL
 
-  ann_row = data.wide[,1:6]
-  row.names(ann_row) = data.wide$Precursor.Id
+  ann_row = data.s.wide[,1:6]
+  row.names(ann_row) = data.s.wide$Precursor.Id
 
   # collect data in list result object
   qdata$data_long = data.s
